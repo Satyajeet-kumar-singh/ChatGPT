43a3433ce8b92f584e0b40c55b056e3a4ab24a8d
satyajeet-kumar-singh <satyavrajput9898@gmail.com>
2026-09-21 13:03:28 +0530

    file vite proxy configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-
-  server: {
-    proxy: {
-      "/chat": VITE_API_BASE_URL,
-      "/conversations": VITE_API_BASE_URL,
-      "/history": VITE_API_BASE_URL,
-      "/upload": VITE_API_BASE_URL,
-    },
-  }
 })
